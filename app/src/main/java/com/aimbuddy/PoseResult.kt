c2f16356762701a2e91b2fd7ff05e919f8f273d0
duyahn3-spec <duyahn3@gmail.com>
2026-08-25 16:45:46 +0700

    Update PoseResult.kt

--- app/src/main/java/com/aimbuddy/PoseResult.kt
+++ app/src/main/java/com/aimbuddy/PoseResult.kt
@@ -1,15 +1,5 @@
-package com.example.poseresearch
+package com.aimbuddy
 
 data class PoseResult(
-    val timestampMs: Long,
-    val frameWidth: Int,
-    val frameHeight: Int,
-    val centerX: Float,
-    val centerY: Float,
-    val left: Float,
-    val top: Float,
-    val right: Float,
-    val bottom: Float,
-    val keypoints: List<PoseKeypoint>,
-    val latencyMs: Long
+    val keypoints: List<PoseKeypoint>
 )
